0eb8ca6db14bb942db6fc856e39a6647db5fd735
ano-vo <ngoctranvl2006@gmail.com>
2022-11-20 14:30:37 +0700

    Pokemon

---
+++ app.js
@@ -0,0 +1,25 @@
+// const CheckSoChan = (n) => {
+//     if (n%2 == 0){
+//         return true; 
+//     }
+//     else {
+//         return false;
+//     }
+// }
+// const SoChan = () => {
+//     for (let i = 0; i < 100; i++){
+//         if (CheckSoChan(i) == true){
+//             document.writeln(i);
+//         }
+//     }
+// }
+// SoChan();
+
+// const color = ["red", "green", "yellow"];
+// // for (let x of color){
+// //     document.writeln(x);
+// // }
+
+// for (let x in color){
+//     document.writeln(x); 
+// }
